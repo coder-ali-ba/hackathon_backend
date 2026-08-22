e1f25c421146d2651edca60744932e8a66c55ea7
coder-ali-ba <habibali8770@gmail.com>
2026-08-23 01:58:47 +0500

    module fixed

--- server.js
+++ server.js
@@ -11,32 +11,32 @@ dotenv.config()
 
 const app =express()
 
-// app.use(
-//   cors({
-//     origin: "https://hackathon-frontend-chi-two.vercel.app",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://hackathon-frontend-chi-two.vercel.app",
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://hackathon-frontend-chi-two.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://hackathon-frontend-chi-two.vercel.app",
+// ];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 
 
