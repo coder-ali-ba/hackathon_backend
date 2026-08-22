c7fbdfcbd8425aa4c1942f1fff0281b4d7779d6e
coder-ali-ba <habibali8770@gmail.com>
2026-08-22 16:23:00 +0500

    deployed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app =express()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hackathon-frontend-chi-two.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
